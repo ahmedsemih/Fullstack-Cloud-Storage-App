c16c0607c4e01975ae00119d2841f697b4d5d811
Ahmed Semih Erkan <ahmedsemiherkan@gmail.com>
2024-03-21 11:44:51 +0300

    feat: add theme provider and theme toggle

--- src/components/providers/index.tsx
+++ src/components/providers/index.tsx
@@ -1,11 +1,20 @@
 import { ReactNode } from "react";
 import { ClerkProvider } from "@clerk/nextjs";
 
+import { ThemeProvider } from "./ThemeProvider";
+
 const Providers = ({ children }: { children: ReactNode }) => {
   return (
-    <ClerkProvider appearance={{ elements: { footer: "hidden" } }}>
-      {children}
-    </ClerkProvider>
+    <ThemeProvider
+      attribute="class"
+      defaultTheme="system"
+      enableSystem
+      disableTransitionOnChange
+    >
+      <ClerkProvider appearance={{ elements: { footer: "hidden" } }}>
+        {children}
+      </ClerkProvider>
+    </ThemeProvider>
   );
 };
 

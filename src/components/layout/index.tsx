61a00a113d7add333248c087575635fab64fb2cc
Ahmed Semih Erkan <ahmedsemiherkan@gmail.com>
2024-04-21 11:24:39 +0300

    feat: modify col count on xl screen width

--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -5,9 +5,9 @@ import SideMenu from "./SideMenu";
 
 const Layout = ({ children }: { children: ReactNode }) => {
   return (
-    <main className="px-2 sm:px-0 grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-6 min-h-screen">
+    <main className="px-2 sm:px-0 grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 min-h-screen">
       <SideMenu />
-      <div className="col-span-1 md:col-span-2 lg:col-span-3 xl:col-span-5 px-2 sm:px-8">
+      <div className="col-span-1 md:col-span-2 lg:col-span-3 xl:col-span-4 2xl:col-span-5 px-2 sm:px-8">
         <Topbar />
         {children}
       </div>
